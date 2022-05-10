acbafc8951423497abe4f006ff6621531b58ad44
C1489-MuskaanSolkar <105113061+C1489-MuskaanSolkar@users.noreply.github.com>
2022-05-10 12:03:13 +0530

    Add files via upload

---
+++ arithmetic.java
@@ -0,0 +1,61 @@
+package arithmetic;
+import java.util.Scanner;
+public class arithmetic {
+
+	public static void main(String[] args) {
+	int num1,num2,choice,res;
+Scanner sc =new Scanner(System.in);
+
+
+System.out.println("Enter your choice");
+choice=sc.nextInt();
+System.out.println("1 + \n 2 - \n 3 * \n 4 / \n 5 %");
+
+switch(choice) {
+case 1:
+	System.out.println("Enter two number");
+	num1=sc.nextInt();
+	num2=sc.nextInt();
+	res=num1+num2;
+	System.out.println("Addition of two numbers are:" +res);
+	break;
+	
+case 2:
+	System.out.println("Enter two number");
+	num1=sc.nextInt();
+	num2=sc.nextInt();
+	res=num1-num2;
+	System.out.println("Subtraction of two numbers are:" +res);
+	break;
+	
+case 3:
+	System.out.println("Enter two number");
+	num1=sc.nextInt();
+	num2=sc.nextInt();
+	res=num1*num2;
+	System.out.println("Multiplication of two numbers are:" +res);
+	break;
+	
+case 4:
+	System.out.println("Enter two number");
+	num1=sc.nextInt();
+	num2=sc.nextInt();
+	res=num1/num2;
+	System.out.println("Division of two numbers are:" +res);
+	break;
+	
+case 5:
+	System.out.println("Enter two number");
+	num1=sc.nextInt();
+	num2=sc.nextInt();
+	res=num1%num2;
+	System.out.println("mod of two numbers are:" +res);
+	break;
+	
+	default:
+		System.out.println("It is invalid");
+		break;
+}
+	}
+
+}
